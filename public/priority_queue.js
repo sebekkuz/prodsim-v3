9d4ccaa10bab40e32ad5488bd7f2ac7e6588b468
sebekkuz <sebekkuz@gmail.com>
2025-12-13 01:08:54 +0100

    tapu 1, Podpunkt 2: Determinizm Symulacji.

--- public/priority_queue.js
+++ public/priority_queue.js
@@ -1,12 +1,17 @@
 // priority_queue.js
-// Implementacja Kolejki Priorytetowej (Min-Heap)
-// To jest znacznie wydajniejsze (O(log n)) niż Array.splice (O(n)) z V1
-// dla zarządzania kolejką zdarzeń symulacji.
+// Klasyczna implementacja Min-Heap (Kopiec Minimalny)
+// Używana do zarządzania zdarzeniami w symulacji Discrete Event Simulation (DES).
 
 class PriorityQueue {
+    /**
+     * @param {function} comparator - Funkcja porównująca (domyślnie a.time < b.time)
+     */
     constructor(comparator = (a, b) => a.time < b.time) {
         this.heap = [];
-        this.comparator = comparator; // Porównuje elementy, np. zdarzenia po czasie
+        this.comparator = comparator;
+        // Licznik sekwencyjny dla zapewnienia determinizmu (stabilności) kolejki
+        // Jeśli czasy są identyczne, decyduje kolejność dodania.
+        this.sequenceCounter = 0;
     }
 
     size() {
@@ -17,45 +22,81 @@ class PriorityQueue {
         return this.size() === 0;
     }
 
-    // Zwraca szczyt (najbliższe zdarzenie) bez usuwania
     peek() {
-        return this.heap[0] || null;
+        return this.heap[0]?.value || null;
     }
 
-    // Dodaje nowy element (zdarzenie)
+    /**
+     * Dodaje element do kolejki.
+     * @param {any} value - Wartość (np. obiekt zdarzenia)
+     */
     push(value) {
-        this.heap.push(value);
+        this.sequenceCounter++;
+        // Pakujemy wartość w obiekt węzła z unikalnym ID sekwencyjnym
+        const node = { 
+            value, 
+            seq: this.sequenceCounter 
+        };
+        this.heap.push(node);
         this.siftUp(this.heap.length - 1);
     }
 
-    // Usuwa i zwraca szczyt (najbliższe zdarzenie)
+    /**
+     * Usuwa i zwraca element o najwyższym priorytecie (najmniejszym czasie).
+     * @returns {any}
+     */
     pop() {
         if (this.isEmpty()) return null;
-        if (this.size() === 1) return this.heap.pop();
+        
+        // Jeśli tylko jeden element, po prostu go usuń
+        if (this.size() === 1) {
+            return this.heap.pop().value;
+        }
 
-        const poppedValue = this.heap[0];
+        const poppedNode = this.heap[0];
+        // Przenieś ostatni element na szczyt
         this.heap[0] = this.heap.pop();
+        // Przywróć własność kopca
         this.siftDown(0);
-        return poppedValue;
+        
+        return poppedNode.value;
     }
 
-    // === Metody wewnętrzne (helpers) ===
+    /**
+     * Wewnętrzna metoda porównująca dwa węzły (Node).
+     * Uwzględnia logikę biznesową (czas) ORAZ determinizm (seq).
+     */
+    compareNodes(nodeA, nodeB) {
+        const valA = nodeA.value;
+        const valB = nodeB.value;
 
-    parent(i) {
-        return Math.floor((i - 1) / 2);
+        // 1. Sprawdź główny warunek (np. czas) używając przekazanego komparatora
+        // Uwaga: Komparator zwraca true jeśli A ma wyższy priorytet (jest mniejszy) niż B
+        // Musimy to obsłużyć ostrożnie. Standardowy komparator to (a,b) => a.time < b.time
+        
+        // Pobieramy czasy bezpośrednio dla pewności (zakładamy strukturę {time: ...})
+        const timeA = valA.time;
+        const timeB = valB.time;
+
+        if (timeA !== timeB) {
+            return timeA < timeB;
+        }
+
+        // 2. Tie-breaker: Jeśli czasy są równe, decyduje kolejność dodania (FIFO)
+        // Mniejszy numer sekwencyjny = dodany wcześniej = wyższy priorytet
+        return nodeA.seq < nodeB.seq;
     }
-    leftChild(i) {
-        return 2 * i + 1;
-    }
-    rightChild(i) {
-        return 2 * i + 2;
-    }
+
+    parent(i) { return Math.floor((i - 1) / 2); }
+    leftChild(i) { return 2 * i + 1; }
+    rightChild(i) { return 2 * i + 2; }
+    
     swap(i, j) {
         [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
     }
 
     siftUp(i) {
-        while (i > 0 && this.comparator(this.heap[i], this.heap[this.parent(i)])) {
+        while (i > 0 && this.compareNodes(this.heap[i], this.heap[this.parent(i)])) {
             this.swap(i, this.parent(i));
             i = this.parent(i);
         }
@@ -66,10 +107,10 @@ class PriorityQueue {
         const left = this.leftChild(i);
         const right = this.rightChild(i);
 
-        if (left < this.size() && this.comparator(this.heap[left], this.heap[maxIndex])) {
+        if (left < this.size() && this.compareNodes(this.heap[left], this.heap[maxIndex])) {
             maxIndex = left;
         }
-        if (right < this.size() && this.comparator(this.heap[right], this.heap[maxIndex])) {
+        if (right < this.size() && this.compareNodes(this.heap[right], this.heap[maxIndex])) {
             maxIndex = right;
         }
 
